8234cc0cfc27aeced0f9f83c077e9719a0827751
saveetha17 <saveethakathiravan@gmail.com>
2023-06-08 16:12:10 -0400

    models done

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,38 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
 
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreignKey: 'product_id',
+  },
+
+  as: 'product_tags'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreignKey: 'tag_id',
+  },
+ 
+  as: 'tag_products'
+});
 
 module.exports = {
   Product,
